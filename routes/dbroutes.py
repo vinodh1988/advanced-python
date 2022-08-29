2bd0907016953d1db8c73c11591247e1cadde1f9
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-29 12:32:08 +0530

    sql-route-commit

---
+++ routes/dbroutes.py
@@ -0,0 +1,24 @@
+from config import db,app,config
+from models import Person
+from flask import jsonify,request,abort
+
+@app.route("/people")
+def getPeople():
+    listp=Person.query.all()
+    result=[x.serialize() for x in listp]
+    return jsonify(result)
+
+
+@app.route('/people',methods=['POST'])
+def addPeople():
+    try:
+        input=request.get_json()
+        sno=input['sno']
+        name=input['name']
+        city=input['city']
+        db.session.add(Person(sno,name,city))
+        db.session.commit()
+        return {"status":"success"}, 201
+    
+    except:
+        abort ({"status":"server error"},500)

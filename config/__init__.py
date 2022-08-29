90f1bd4a3a0d1fb6d6a327c4135ba7793248385f
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-29 11:57:32 +0530

    db-commit

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir =os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.db')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db=SQLAlchemy(app)

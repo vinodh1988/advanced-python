0146fcd2853d7f9f2176b86668603524fe0577a7
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-29 11:39:57 +0530

    new-changes

--- routes/approutes.py
+++ routes/approutes.py
@@ -4,3 +4,7 @@ from config import app
 @app.get("/hello")
 def hello():
     return "Flass app is running!!! Hello !!!!"
+
+@app.get("/greet")
+def greet():
+    return "Hey! How are you"
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from config import app
+import routes
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)

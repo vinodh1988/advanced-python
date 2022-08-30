c6cc3c3043c1ae18e511824844b2723f77b796be
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-30 12:11:12 +0530

    401

--- routes/dbroutes.py
+++ routes/dbroutes.py
@@ -1,8 +1,10 @@
 from config import db,app
 from models import Person
 from flask import jsonify,request,abort
+from security import auth
 
 @app.route("/people")
+@auth.login_required
 def getPeople():
     listp=Person.query.all()
     result=[x.serialize() for x in listp]
@@ -10,6 +12,7 @@ def getPeople():
 
 
 @app.route('/people',methods=['POST'])
+@auth.login_required
 def addPeople():
     try:
         input=request.get_json()

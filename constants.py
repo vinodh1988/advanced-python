60f120cb3fc3006d96e80f37eca08cac61326d91
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-01 12:49:21 +0530

    asyncio

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ import logging
 
 configdir =os.path.abspath(os.path.dirname(__file__))+"/config"
 
-logging.basicConfig(filename=configdir+"/app.log",filemode='a',format="%(asctime)s  %(process)d-%(levelname)s-%(message)s")
+logging.basicConfig(filename=configdir+"/app.log",filemode='a',format="%(asctime)s  %(process)d-%(levelname)s-%(message)s",level=logging.INFO)
